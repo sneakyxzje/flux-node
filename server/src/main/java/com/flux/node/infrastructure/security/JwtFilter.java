32e52eace0acd9d9510f9b68bcbd5ed531073b9c
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-23 12:34:45 +0700

    feat(infrastructure): add jwt filter

---
+++ server/src/main/java/com/flux/node/infrastructure/security/JwtFilter.java
@@ -0,0 +1,64 @@
+package com.flux.node.infrastructure.security;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractToken(req);
+        if(token == null) {
+            filterChain.doFilter(req, resp);
+            return;
+        }
+
+        if(!jwtProvider.validateAccessToken(token)) {
+            filterChain.doFilter(req, resp);
+            return;
+        }
+
+        Long userId = jwtProvider.getUserIdFromToken(token);
+        List<String> roles = jwtProvider.getRolesFromToken(token);
+
+        List<SimpleGrantedAuthority> authorities = roles.stream()
+        .map(SimpleGrantedAuthority::new)
+        .collect(Collectors.toList());
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId,null, authorities);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(req, resp);
+    }
+
+    private String extractToken(HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if("accessToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
