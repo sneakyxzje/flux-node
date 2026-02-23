104616ef193b1111e3ae20d63993ce72363e248a
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-23 12:34:04 +0700

    feat(infrastructure): implement user repository

---
+++ server/src/main/java/com/flux/node/infrastructure/persistence/repository/Impl/user/UserRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.flux.node.infrastructure.persistence.repository.Impl.user;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.flux.node.domain.model.User;
+import com.flux.node.domain.repository.UserRepository;
+import com.flux.node.infrastructure.persistence.entity.UserEntity;
+import com.flux.node.infrastructure.persistence.mapper.UserMapper;
+import com.flux.node.infrastructure.persistence.repository.JPA.user.JPAUserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+    private final JPAUserRepository jpaUserRepository;
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        Optional<UserEntity> entityOpt = jpaUserRepository.findByEmail(email);
+        return entityOpt.map(UserMapper::toDomain);
+    }
+
+    @Override
+    public User save(User user) {
+        UserEntity entity = UserMapper.toEntity(user);
+        
+        UserEntity saved = jpaUserRepository.save(entity);
+        
+        return UserMapper.toDomain(saved);
+    }
+}
