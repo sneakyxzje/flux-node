c4d92b7f0fdf945ad85c0769ff2de1f4d889e205
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-24 18:56:20 +0700

    feat(port): extract userid from token

--- server/src/main/java/com/flux/node/application/port/TokenService.java
+++ server/src/main/java/com/flux/node/application/port/TokenService.java
@@ -1,8 +1,11 @@
 package com.flux.node.application.port;
 
+
 public interface TokenService {
     String generateAccessToken(String userId, String username, String role, String sId);
     String generateSessionId();
     String generateJti();
     String generateRefreshToken();
+    Long getUserIdFromToken(String accessToken);
 }
+
