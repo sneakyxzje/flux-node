fc2854994fa9b5d2b3a2ff5e782e9383e5ca1ab8
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-24 18:55:08 +0700

    feat(port): expand refresh token repository with metadata and retrieval

--- server/src/main/java/com/flux/node/application/port/RefreshTokenRepository.java
+++ server/src/main/java/com/flux/node/application/port/RefreshTokenRepository.java
@@ -1,7 +1,12 @@
 package com.flux.node.application.port;
 
+import java.util.Optional;
+
+import com.flux.node.application.dto.RefreshTokenData;
+
 public interface RefreshTokenRepository {
-    void save(String refreshToken, Long userId, String sId, String jTi, long duration);
-    
+    void save(String refreshToken, Long userId,String username, String role, String sId, String jTi, long duration);
+    Optional<RefreshTokenData> findByRefreshToken(String refreshToken);    
     void delete(String refreshToken);
 }
+
