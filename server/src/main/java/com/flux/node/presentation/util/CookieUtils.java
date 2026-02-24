b55dc1048e524a6ff22301a599c28ea556e483a3
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-24 18:59:05 +0700

    feat(util): add cookie utils for request cookie extraction

---
+++ server/src/main/java/com/flux/node/presentation/util/CookieUtils.java
@@ -0,0 +1,14 @@
+package com.flux.node.presentation.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class CookieUtils {
+    public static String extract(HttpServletRequest request, String name) {
+        if(request.getCookies() == null) return null;
+        for(Cookie cookie : request.getCookies()) {
+            if(name.equals(cookie.getName())) return cookie.getValue();
+        }
+        return null;
+    } 
+}
