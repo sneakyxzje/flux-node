f94303e1d785cff1982d194c9bfe516d71781c0c
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-24 14:54:44 +0700

    feat(port): add refresh token repository interface

---
+++ server/src/main/java/com/flux/node/application/port/RefreshTokenRepository.java
@@ -0,0 +1,6 @@
+package com.flux.node.application.port;
+
+public interface RefreshTokenRepository {
+    void save(String refreshToken, Long userId, long duration);
+    void delete(String refreshToken);
+}
