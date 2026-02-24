25faed30cf100756ed3103ad7b5a6ba65da79043
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-24 14:58:13 +0700

    feat(domain): add find by username method

--- server/src/main/java/com/flux/node/domain/repository/UserRepository.java
+++ server/src/main/java/com/flux/node/domain/repository/UserRepository.java
@@ -6,5 +6,6 @@ import com.flux.node.domain.model.User;
 
 public interface UserRepository {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
     User save(User user);
 }
