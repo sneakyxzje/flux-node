aa96268624ca393493c4ac7643767cc48f01357a
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-25 18:17:40 +0700

    feat(service): implement generic http and attempt-based error handling

---
+++ www/src/lib/services/http_client.ts
@@ -0,0 +1,72 @@
+import { attempt, type Attempt } from '@duydang2311/attempt';
+
+export interface HttpClientOptions {
+	fetcher: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+	headers: HeadersInit;
+	baseUrl: string;
+}
+
+export class HttpClient {
+	#options: HttpClientOptions;
+	constructor(baseUrl: string, options?: HttpClientOptions) {
+		this.#options = {
+			baseUrl: baseUrl,
+			fetcher: options?.fetcher || fetch,
+			headers: options?.headers || {}
+		};
+	}
+	async get<TResponse, TError = Error>(endpoint: string): Promise<Attempt<TResponse, TError>> {
+		return this.#request(() => this.prepare(endpoint, 'GET'));
+	}
+
+	public async post<TRequest, TResponse, TError = Error>(
+		endpoint: string,
+		body: TRequest
+	): Promise<Attempt<TResponse, TError>> {
+		return this.#request(() => this.prepare<TRequest, TResponse>(endpoint, 'POST', body));
+	}
+
+	async prepare<TRequest, TResponse>(
+		endpoint: string,
+		method: string,
+		body?: TRequest,
+		headers?: HeadersInit
+	): Promise<TResponse> {
+		const url = `${this.#options.baseUrl}${endpoint}`;
+		const finalHeaders = new Headers(this.#options.headers);
+		if (headers) {
+			new Headers(headers).forEach((v, k) => finalHeaders.set(k, v));
+		}
+		let finalBody: string | undefined = undefined;
+
+		if (body) {
+			finalBody = this.#serializeBody(body);
+			finalHeaders.set('Content-Type', 'application/json');
+		}
+
+		const response = await this.#options.fetcher(url, {
+			method,
+			headers: finalHeaders,
+			body: finalBody,
+			credentials: 'include'
+		});
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		return response.json() as Promise<TResponse>;
+	}
+
+	async #request<TRes, TError = Error>(
+		action: () => Promise<TRes>
+	): Promise<Attempt<TRes, TError>> {
+		return await attempt.async(action)((e: unknown) => e as TError);
+	}
+
+	#serializeBody = (data: unknown) => {
+		if (typeof data === 'string') return data;
+		if (typeof data === 'object') {
+			return JSON.stringify(data);
+		}
+		return undefined;
+	};
+}
