15b5089ee02be514723ac496565da72c0341a4fd
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-25 18:08:00 +0700

    feat(public): simple navbar for landing page

---
+++ www/src/lib/components/ui/Navbar.svelte
@@ -0,0 +1,23 @@
+<header
+	class="fixed top-4 right-4 left-4 z-50 rounded-full border border-border bg-background/90 shadow-sm backdrop-blur-md"
+>
+	<div class="mx-auto flex h-14 items-center justify-between px-4 sm:px-6 lg:px-8">
+		<a href="/" class="text text-xl font-bold tracking-tight uppercase transition-colors"> Flux </a>
+		<div class="flex items-center gap-4">
+			<a
+				href="/login"
+				class="
+                    rounded-md bg-teal-600
+                    px-4 py-2
+                    text-sm
+                    font-semibold text-primary-foreground
+                    shadow-md
+                    transition-all duration-200
+                     hover:shadow-lg
+                "
+			>
+				Bắt đầu ngay
+			</a>
+		</div>
+	</div>
+</header>
---
+++ www/src/routes/(public)/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Navbar from '$lib/components/ui/Navbar.svelte';
+</script>
+
+<Navbar />
