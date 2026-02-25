52d3a5691a9ee648fdf9f0e7235092756b5d1aef
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-25 18:16:13 +0700

    feat(i18n): integrate paraglide for internationalization support

--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
+
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -6,4 +9,7 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
