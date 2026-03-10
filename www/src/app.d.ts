2393731fbb6bcf63bd81bdbbaa04cd03b8ae042c
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-10 18:43:19 +0700

    feat(types): add api and user to app locals

--- www/src/app.d.ts
+++ www/src/app.d.ts
@@ -1,13 +1,17 @@
-import type { AvailableLanguageTag } from "../../lib/paraglide/runtime"
-import type { ParaglideLocals } from "@inlang/paraglide-sveltekit"
+import type { User } from '$lib/models/user';
+import type { HttpClient } from '$lib/services/http_client';
+import type { AvailableLanguageTag } from '../../lib/paraglide/runtime';
+import type { ParaglideLocals } from '@inlang/paraglide-sveltekit';
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		 interface Locals {
-    paraglide: ParaglideLocals<AvailableLanguageTag>,
-}
+		interface Locals {
+			paraglide: ParaglideLocals<AvailableLanguageTag>;
+			api: HttpClient;
+			user: User;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
