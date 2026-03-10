a0ca80c9c1e10a1654ca421e2bff753568196adf
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-10 18:48:45 +0700

    feat(delivery): simple delivery page

---
+++ www/src/routes/(private)/delivery/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as m from '$lib/paraglide/messages.js';
+</script>
+
+<div class="flex justify-between">
+	<h1>Delivery</h1>
+
+	<a href="/delivery/create">
+		<Button class="bg-primary font-bold text-base-fg-1">
+			{m.create_shipment_title()}
+		</Button>
+	</a>
+</div>
