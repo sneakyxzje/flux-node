7514644ce0271b26d639dafa4fde4e8412715f72
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-10 18:35:14 +0700

    feat(infrastructure): secured /me endpoint

--- server/src/main/java/com/flux/node/infrastructure/configuration/SecurityConfig.java
+++ server/src/main/java/com/flux/node/infrastructure/configuration/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/me").authenticated()
                 .requestMatchers("/api/v1/auth/**").permitAll() 
                 .anyRequest().authenticated()
             )
