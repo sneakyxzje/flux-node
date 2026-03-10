7279bdf30aaae99c03fae1d534458b4431f686dd
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-10 18:34:54 +0700

    feat(infrastructure): add get me use case and shipment use case to bean config

--- server/src/main/java/com/flux/node/infrastructure/configuration/BeanConfig.java
+++ server/src/main/java/com/flux/node/infrastructure/configuration/BeanConfig.java
@@ -7,9 +7,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.flux.node.application.port.RefreshTokenRepository;
 import com.flux.node.application.port.TokenService;
-import com.flux.node.application.usecase.LoginUseCase;
-import com.flux.node.application.usecase.RefreshUseCase;
-import com.flux.node.application.usecase.RegisterUseCase;
+import com.flux.node.application.usecase.auth.GetMeUseCase;
+import com.flux.node.application.usecase.auth.LoginUseCase;
+import com.flux.node.application.usecase.auth.RefreshUseCase;
+import com.flux.node.application.usecase.auth.RegisterUseCase;
+import com.flux.node.application.usecase.shipment.ShipmentUseCase;
+import com.flux.node.domain.repository.ShipmentRepository;
 import com.flux.node.domain.repository.UserRepository;
 
 
@@ -27,9 +30,18 @@ public class BeanConfig {
     }
 
     @Bean
+    public GetMeUseCase getMeUseCase(UserRepository repo) {
+        return new GetMeUseCase(repo);
+     }
+    @Bean
     public RefreshUseCase refreshUseCase(RefreshTokenRepository refreshTokenRepository, TokenService tokenService,
         @Value("${jwt.refresh-token-expiration}") long refreshTokenDuration
     ) {
         return new RefreshUseCase(refreshTokenRepository, tokenService, refreshTokenDuration);
+    }
+
+    @Bean
+    public ShipmentUseCase shipmentUseCase(ShipmentRepository repo) {
+        return new ShipmentUseCase(repo);
     }
 }
