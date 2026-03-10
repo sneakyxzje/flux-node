9eaa85db7767676d871ee3c4e7fbc211539ebdb6
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-10 18:34:16 +0700

    feat(domain): add find by id method

--- server/src/main/java/com/flux/node/domain/repository/UserRepository.java
+++ server/src/main/java/com/flux/node/domain/repository/UserRepository.java
@@ -6,6 +6,7 @@ import com.flux.node.domain.model.User;
 
 public interface UserRepository {
     Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
     Optional<User> findByUsername(String username);
     User save(User user);
 }
